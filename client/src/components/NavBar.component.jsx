201da77368f8b613299f3eeeb556225bc96c5b38
Toba-dean <tobaogundimu@gmail.com>
2023-06-01 18:53:32 +0100

    done the nav bar component

--- client/src/components/NavBar.component.jsx
+++ client/src/components/NavBar.component.jsx
@@ -25,10 +25,11 @@ const NavBar = () => {
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
+  // const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  // const fullName = `${user?.firstName} ${user?.lastName}`;
+  const fullName = `Dean`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
@@ -40,14 +41,160 @@ const NavBar = () => {
           onClick={() => navigate("/home")}
           sx={{
             "&:hover": {
-              color: primaryLight,
+              opacity: 0.7,
               cursor: "pointer",
             },
           }}
         >
           Sociopedia
         </Typography>
+
+        {
+          isNonMobileScreens && (
+            <FlexBetween
+              backgroundColor={neutralLight}
+              borderRadius="9px"
+              gap="3rem"
+              padding="0.1rem 1.5rem"
+            >
+              <InputBase placeholder="Search..." />
+              <IconButton>
+                <Search />
+              </IconButton>
+            </FlexBetween>
+          )
+        }
       </FlexBetween>
+
+      {/* Desktop Nav */}
+      {
+        isNonMobileScreens ? (
+          <FlexBetween gap="2rem">
+            <IconButton onClick={() => dispatch(SET_MODE())}>
+              {
+                theme.palette.mode === "dark" ? (
+                  <DarkMode sx={{ fontSize: "25px" }} />
+                ) : (
+                  <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                )
+              }
+            </IconButton>
+
+            <Message sx={{ fontSize: "25px" }} />
+            <Notifications sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(SET_LOGOUT())}>Log Out</MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        ) : (
+          <IconButton
+            onClick={() => setIsMobileMenuToggled(isMobileMenuToggled => !isMobileMenuToggled)}
+          >
+            <Menu />
+          </IconButton>
+        )
+      }
+
+      {/* Mobile Nav */}
+      {
+        !isNonMobileScreens && isMobileMenuToggled && (
+          <Box
+            position="fixed"
+            right="0"
+            bottom="0"
+            height="100%"
+            zIndex="10"
+            maxWidth="500px"
+            minWidth="300px"
+            backgroundColor={background}
+          >
+            {/* CLOSE ICON */}
+            <Box display="flex" justifyContent="flex-end" p="1rem">
+              <IconButton
+                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+              >
+                <Close />
+              </IconButton>
+            </Box>
+
+            {/* MENU ITEMS */}
+            <FlexBetween
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              gap="3rem"
+            >
+              <IconButton
+                onClick={() => dispatch(SET_MODE())}
+                sx={{ fontSize: "25px" }}
+              >
+                {
+                  theme.palette.mode === "dark" ? (
+                    <DarkMode sx={{ fontSize: "25px" }} />
+                  ) : (
+                    <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                  )
+                }
+              </IconButton>
+
+              <Message sx={{ fontSize: "25px" }} />
+              <Notifications sx={{ fontSize: "25px" }} />
+              <Help sx={{ fontSize: "25px" }} />
+
+              <FormControl variant="standard" value={fullName}>
+                <Select
+                  value={fullName}
+                  sx={{
+                    backgroundColor: neutralLight,
+                    width: "150px",
+                    borderRadius: "0.25rem",
+                    p: "0.25rem 1rem",
+                    "& .MuiSvgIcon-root": {
+                      pr: "0.25rem",
+                      width: "3rem",
+                    },
+                    "& .MuiSelect-select:focus": {
+                      backgroundColor: neutralLight,
+                    },
+                  }}
+                  input={<InputBase />}
+                >
+                  <MenuItem value={fullName}>
+                    <Typography>{fullName}</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={() => dispatch(SET_LOGOUT())}>
+                    Log Out
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </FlexBetween>
+          </Box>
+        )
+      }
     </FlexBetween>
   )
 }
--- client/src/containers/HomePage.container.jsx
+++ client/src/containers/HomePage.container.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Box } from "@mui/material";
+
+import { NavBar } from "components";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box>
+      <NavBar />
+    </Box>
   )
 }
 
