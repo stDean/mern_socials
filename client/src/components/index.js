7caf6c4424ad55992c380524af91647a51e87663
Toba-dean <tobaogundimu@gmail.com>
2023-06-02 18:52:24 +0100

    added the user profile widget to the home page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,5 @@
 export { default as NavBar } from "./NavBar.component";
-export { default as UserImage } from "./UserImage.component";
 export { default as Friends } from "./Friends.component"
-export { default as FlexBetween } from "../helpers/FlexBetween.helper";
 export { default as Form } from "./Form.component";
 export { default as NotFound } from "./NotFound.component";
+export { default as UserWidget } from "./widget/User.widget";
--- client/src/containers/HomePage.container.jsx
+++ client/src/containers/HomePage.container.jsx
@@ -1,11 +1,39 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 
-import { NavBar } from "components";
+import { NavBar, UserWidget } from "components";
 
 const HomePage = () => {
+
+  const { _id, picturePath } = useSelector(({ auth }) => auth.user);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
   return (
     <Box>
       <NavBar />
+
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+
+        {
+          isNonMobileScreens && (
+            <Box flexBasis="26%"></Box>
+          )
+        }
+      </Box>
     </Box>
   )
 }
